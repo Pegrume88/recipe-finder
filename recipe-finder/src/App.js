8a1b409243d2896e5034d4bdc99ae4f6affccfc5
Rhys Pegrume <pegrume88@gmail.com>
2023-08-03 21:22:27 +0200

    App.js functions created

--- recipe-finder/src/App.js
+++ recipe-finder/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import SearchBar from "./Components/SearchBar";
+import RecipeList from "./Components/RecipeList";
+import { searchRecipes } from "./Api";
 
-function App() {
+const App = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  const handleSearch = async (query) => {
+    const results = await searchRecipes(query);
+    setRecipes(results);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Recipe Finder</h1>
+      <SearchBar onSearch={handleSearch} />
+      <RecipeList recipes={recipes} />
     </div>
   );
-}
+};
 
 export default App;
